2e18dae1b6def20e28fb79b1bc89901558429777
steedos <zhuangjianguo@hotoadeMacBook-Pro.local>
2015-10-18 23:59:56 +0800

    Initial commit

---
+++ dev.sh
@@ -0,0 +1,6 @@
+export ROCKETCHAT_URL=chat.steedos.com:3000
+export ROCKETCHAT_ROOM=GENERAL,RXG6iX48zLH4p8HC5
+export ROCKETCHAT_USER=robot
+export ROCKETCHAT_PASSWORD=
+
+bin/hubot -a rocketchat --name robot --alias bot
